4232aacbfea3f2c431c77cad4ddebc2be2f51f30
Marlon Sttefane Guimarães Silva <marlon.sttefane@hotmail.com>
2025-12-11 17:36:42 -0300

    att

--- SAVe_New/frontend/src/routes/CaseReport.svelte
+++ SAVe_New/frontend/src/routes/CaseReport.svelte
@@ -1357,7 +1357,6 @@
         * {
             -webkit-print-color-adjust: exact !important;
             print-color-adjust: exact !important;
-            color-adjust: exact !important;
         }
 
         /* Remove shadows and unnecessary decorations */
